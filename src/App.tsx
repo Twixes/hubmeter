740c2cb054d004bd4cbeee2a890fba5266abcb66
Twixes <dev@twixes.com>
2020-06-07 19:58:01 +0200

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import './App.scss';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Header/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+function Footer(): JSX.Element {
+  return (
+    <footer className="Footer">
+      <span>GitHub activity insights in terms of time.<br/>
+      By <a href="https://twixes.com">Twixes</a>. 👋</span>
+    </footer>
+  );
+};
+
+
+export default Footer;
