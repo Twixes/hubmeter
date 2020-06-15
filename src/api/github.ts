151db47aa8f4f2159c530841613f9174f72b94c3
Twixes <dev@twixes.com>
2020-06-15 21:06:57 +0200

    Update github.ts

--- src/api/github.ts
+++ src/api/github.ts
@@ -22,25 +22,61 @@ export interface Event {
   repo: Repo
 }
 
+export interface Problem {
+  message: string
+}
+
 const ROOT_API_URL: string = 'https://api.github.com/'
 
-function buildURL(parts: string[], params?: object): URL {
+export function buildURL(parts: string[], params?: object): URL {
   const url: URL = new URL(`${ROOT_API_URL}${parts.map(encodeURIComponent).join('/')}`)
   if (params) for (const [key, value] of Object.entries(params)) url.searchParams.append(key, value)
   return url
 }
 
-export async function fetchUserEvents(login: string, page = 1): Promise<Event[]> {
-  if (page < 1 || page > 10) throw Error('only pages between 1 and 10 (inclusive) can be fetched')
-  const url: URL = buildURL(['users', login, 'events'], { page: page })
-  const response: Response = await fetch(url.toString())
-  const events: Event[] = await response.json()
-  return events
+export async function fetchUserEventsPage(login: string, page: number): Promise<Event[]> {
+    const url: URL = buildURL(['users', login, 'events'])
+    url.searchParams.set('page', page.toString())
+    const response: Response = await fetch(url.toString())
+    if (response.status === 403) {
+      const rateLimitResetEpoch: number = parseInt(response.headers.get('X-Ratelimit-Reset')!) * 1000
+      const rateLimitReset: Date = new Date(rateLimitResetEpoch)
+      const deltaMilliseconds: number = rateLimitResetEpoch - Date.now()
+      throw Error(
+        `GitHub rate limit exceeded for now. Reset at ${rateLimitReset.getHours()}:${rateLimitReset.getMinutes()} ` +
+        `(in ${Math.round(deltaMilliseconds / 1000 / 60)} min).`
+      )
+    } else if (!response.ok) {
+      const problem: Problem = await response.json()
+      throw Error(problem.message)
+    }
+    const events: Event[] = await response.json()
+    return events
+}
+
+export async function fetchUserEventsAll(login: string): Promise<Event[]> {
+  const events: Event[] = []
+  const promises: Promise<Event[]>[] = []
+  for (let page = 1; page <= 10; page++) promises.push(fetchUserEventsPage(login, page))
+  const pages: Event[][] = await Promise.all(promises)
+  return pages.flat()
 }
 
 export async function fetchSearchUsers(query: string): Promise<User[]> {
   const url: URL = buildURL(['search', 'users'], { q: query })
   const response: Response = await fetch(url.toString())
+  if (response.status === 403) {
+    const rateLimitResetEpoch: number = parseInt(response.headers.get('X-Ratelimit-Reset')!) * 1000
+    const rateLimitReset: Date = new Date(rateLimitResetEpoch)
+    const deltaMilliseconds: number = rateLimitResetEpoch - Date.now()
+    throw Error(
+      `GitHub rate limit exceeded for now. Reset at ${rateLimitReset.getHours()}:${rateLimitReset.getMinutes()} ` +
+      `(in ${Math.round(deltaMilliseconds / 1000 / 60)} min).`
+    )
+  } else if (!response.ok) {
+    const problem: Problem = await response.json()
+    throw Error(problem.message)
+  }
   const searchResults: SearchResults = await response.json()
   const users: User[] = searchResults.items
   return users
