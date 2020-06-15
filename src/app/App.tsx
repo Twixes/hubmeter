70aabee963d9f95d2fa705d11e0aec8876655cbf
Twixes <dev@twixes.com>
2020-06-15 23:15:35 +0200

    Rearrange directory structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import './App.scss';
 
 let questions: string[] = [ // "Do ${subject} ${question}?"
