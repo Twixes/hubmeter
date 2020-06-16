363e552abc6a6ac97cc3140ad033abb2f8769a5a
Twixes <dev@twixes.com>
2020-06-17 01:51:27 +0200

    Add sanitize.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from "react-router-dom"
 import './index.scss'
 import App from './app/App'
+import 'sanitize.css'
+import 'sanitize.css/forms.css'
+import 'sanitize.css/typography.css'
 
 ReactDOM.render(
   <React.StrictMode>
