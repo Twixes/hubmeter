d30072349fa31b38e2fa2cf2d295e960b9aa1a40
Twixes <dev@twixes.com>
2020-06-17 03:06:03 +0200

    Improve code style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import './Footer.scss';
 
-function Footer(): JSX.Element {
+export default function Footer(): JSX.Element {
   return (
     <footer className="Footer bounded">
       <span>GitHub activity insights you haven't seen before.<br/>
       By <a href="https://twixes.com">Twixes</a>. 👋</span>
     </footer>
-  );
-};
-
-
-export default Footer;
+  )
+}
