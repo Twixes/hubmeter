da5405c1b1f8b29844307309d2b767fb83eaba45
Twixes <dev@twixes.com>
2020-06-17 02:40:51 +0200

    Make App return a Fragment

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,15 @@ import { Route, Switch } from "react-router-dom"
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Home from '../components/Home'
-import './App.scss'
 
 export default function App(): JSX.Element {
   return (
-    <div className="App">
+    <>
       <Header/>
       <main className="bounded">
         <Route path="/" component={Home} exact={true}/>
       </main>
       <Footer/>
-    </div>
+    </>
   )
 }
