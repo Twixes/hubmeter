8e4fcfd987a104ed2341cda0c4f25ba7a0ab6c5e
Twixes <dev@twixes.com>
2020-06-18 20:34:36 +0200

    Incorporate Home into App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Route, Switch } from "react-router-dom"
+import { Route } from 'react-router-dom'
+import { RootState } from './rootReducer'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import Home from '../components/Home'
+import UserForm from '../components/UserForm'
+
+const QUESTIONS: string[] = [ // "Do ${subject} ${question}?"
+  'release on Friday',
+  'pull all-nighters',
+  'work the night shift',
+  'git up at the crack of dawn',
+  'code on weekends',
+  'code at high noon',
+  'have a life',
+  'ever code'
+]
+
+function HeadlineAbove(): JSX.Element {
+  return <h1>Do</h1>
+}
+
+function HeadlineBelow(): JSX.Element {
+  return <h1>…{QUESTIONS[Math.floor(Math.random() * QUESTIONS.length)]}?</h1>
+}
 
 export default function App(): JSX.Element {
+  const dispatch = useDispatch()
+
+  const { currentLogin } = useSelector(
+    (state: RootState) => state.eventsDisplay
+  )
+
   return (
     <>
       <Header/>
       <main className="bounded">
-        <Route path="/" component={Home} exact={true}/>
+        <Route path="/" component={HeadlineAbove} exact/>
+        <UserForm login={''}/>
+        <Route path="/" component={HeadlineBelow} exact/>
       </main>
       <Footer/>
     </>
