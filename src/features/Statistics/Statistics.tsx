ad35d869436df25979984306f91a3b8d4bf5b6ae
Twixes <dev@twixes.com>
2020-06-20 19:54:52 +0200

    Add Statistics view base

---
+++ src/features/Statistics/Statistics.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil'
+import { motion } from 'framer-motion'
+import { showGHAPIErrorNoticeState, currentUserState, userEventsState } from '../../recoil'
+import { fetchUserEventsAll } from '../../api/github'
+import { Params } from '../../app/App'
+import './Statistics.scss'
+
+export default function Statistics(): JSX.Element {
+  const { login } = useParams<Params>()
+
+  const setShowGHAPIErrorNotice = useSetRecoilState(showGHAPIErrorNoticeState)
+  const currentUser = useRecoilValue(currentUserState)
+  const [userEvents, setUserEvents] = useRecoilState(userEventsState({ login: login!.toLowerCase() }))
+
+  useEffect(() => {
+    if (currentUser && !userEvents) {
+      fetchUserEventsAll(currentUser.login).then(setUserEvents).catch(() => { setShowGHAPIErrorNotice(true)})
+    }
+    console.log(userEvents?.length)
+  }, [setShowGHAPIErrorNotice, currentUser, userEvents, setUserEvents])
+
+  return (
+    <motion.div className="Statistics" positionTransition>
+    </motion.div>
+  )
+}
