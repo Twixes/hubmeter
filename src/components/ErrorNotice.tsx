ca5e1ceaaab88148a8b22ab15869cba1b25d8d72
Twixes <dev@twixes.com>
2020-06-21 02:50:59 +0200

    Improve ErrorNotice animation

--- src/components/ErrorNotice.tsx
+++ src/components/ErrorNotice.tsx
@@ -4,18 +4,29 @@ import { AnimatePresence, motion, useReducedMotion, Variants } from 'framer-moti
 import { showGHAPIErrorNoticeState } from '../atoms'
 import './ErrorNotice.scss'
 
-const VARIANTS: Variants = {
+const OUTSIDE_VARIANTS: Variants = {
   hidden: (shouldReduceMotion: boolean) => {
     return {
+      maxHeight: shouldReduceMotion ? '12rem' : '3rem',
       marginTop: shouldReduceMotion ? '0.75rem' : '-3rem',
       opacity: 0
     }
   },
-  shown: () => {
+  shown: {
+    maxHeight: '12rem',
+    marginTop: '0.75rem',
+    opacity: 1
+  }
+}
+
+const INSIDE_VARIANTS: Variants = {
+  hidden: (shouldReduceMotion: boolean) => {
     return {
-      marginTop: '0.75rem',
-      opacity: 1
+      maxHeight: shouldReduceMotion ? '12rem' : '3rem',
     }
+  },
+  shown: {
+    maxHeight: '12rem',
   }
 }
 
@@ -27,18 +38,21 @@ export default function ErrorNotice(): JSX.Element | null {
     <AnimatePresence>
       {!showGHAPIErrorNotice ? null : (
         <motion.div
-          className="ErrorNotice" custom={shouldReduceMotion} variants={VARIANTS}
+          className="ErrorNotice" custom={shouldReduceMotion} variants={OUTSIDE_VARIANTS}
           initial="hidden" animate="shown" exit="hidden" positionTransition
         >
           <div className="ErrorNotice-indicator">!</div>
-          <div className="ErrorNotice-message">
+          <motion.div
+            className="ErrorNotice-message" custom={shouldReduceMotion} variants={INSIDE_VARIANTS}
+            initial="hidden" animate="shown" exit="hidden"
+          >
             GitHub API error. <a
               href="https://developer.github.com/v3/#rate-limiting" target="_blank" rel="noopener noreferrer"
             >Rate limiting</a> may be at fault. Try again later.
-          </div>
-          <button className="ErrorNotice-button" type="button" onClick={() => { setShowGHAPIErrorNotice(false) }}>
-            ✗
-          </button>
+          </motion.div>
+          <button
+            className="ErrorNotice-button" type="button" onClick={() => { setShowGHAPIErrorNotice(false) }}
+          >✗</button>
         </motion.div>
       )}
     </AnimatePresence>
