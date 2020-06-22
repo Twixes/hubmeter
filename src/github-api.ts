a88c2949671ecf03fb8a96909d87b57bae789624
Twixes <dev@twixes.com>
2020-06-22 19:50:46 +0200

    Add back GitHub API error handling

--- src/github-api.ts
+++ src/github-api.ts
@@ -41,11 +41,27 @@ export interface Event {
   created_at: Date // eslint-disable-line camelcase
 }
 
-export interface Problem {
+interface Problem {
   message: string
 }
 
 const ROOT_API_URL: string = 'https://api.github.com/'
+
+async function checkForProblem(response: Response, ignoreStatuses: number[] = []): Promise<void> {
+  if (response.status === 403) {
+    const rateLimitResetEpoch: number = parseInt(response.headers.get('X-Ratelimit-Reset')!) * 1000
+    const rateLimitReset: Date = new Date(rateLimitResetEpoch)
+    const deltaMinutes: number = Math.ceil((rateLimitResetEpoch - Date.now()) / 60_000)
+    throw Error(
+      `GitHub rate limit exceeded for now. Try again in ${deltaMinutes} min ` +
+      `at ${rateLimitReset.getHours() % 12 || 12}:${rateLimitReset.getMinutes().toString().padStart(2, '0')} ` +
+      `${rateLimitReset.getHours() >= 12 ? 'PM' : 'AM'}.`
+    )
+  } else if (!response.ok && !ignoreStatuses.includes(response.status)) {
+    const problem: Problem = await response.json()
+    throw Error(problem.message)
+  }
+}
 
 export function buildURL(parts: string[], params?: object): URL {
   const url: URL = new URL(`${ROOT_API_URL}${parts.map(encodeURIComponent).join('/')}`)
@@ -57,6 +73,7 @@ export async function fetchUserEventsPage(login: string, page: number): Promise<
   const url: URL = buildURL(['users', login, 'events'])
   url.searchParams.set('page', page.toString())
   const response: Response = await fetch(url.toString())
+  await checkForProblem(response)
   const events: Event[] = await response.json()
   const eventsWithDates: Event[] = events.map(event => {
     return { ...event, created_at: new Date(event.created_at) }
@@ -68,6 +85,7 @@ export async function fetchUserEventsPilot(login: string): Promise<[Event[], num
   const url: URL = buildURL(['users', login, 'events'])
   url.searchParams.set('page', '1')
   const response: Response = await fetch(url.toString())
+  await checkForProblem(response)
   const events: Event[] = await response.json()
   const eventsWithDates: Event[] = events.map(event => {
     return { ...event, created_at: new Date(event.created_at) }
@@ -89,15 +107,17 @@ export async function fetchUserEventsAll(login: string): Promise<Event[]> {
 export async function fetchSearchUsers(query: string): Promise<User[]> {
   const url: URL = buildURL(['search', 'users'], { q: query })
   const response: Response = await fetch(url.toString())
+  await checkForProblem(response)
   const searchResults: SearchResults = await response.json()
   const users: User[] = searchResults.items
   return users
 }
 
-export async function fetchUser(login: string): Promise<User | null> {
+export async function fetchUser(login: string): Promise<User> {
   const url: URL = buildURL(['users', login])
   const response: Response = await fetch(url.toString())
-  if (response.status === 404) return null
+  await checkForProblem(response, [404])
+  if (response.status === 404) throw Error(`User ${login} doesn't exist.`)
   const user: User = await response.json()
   return user
 }
