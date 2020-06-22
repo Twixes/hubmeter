9c0a6d70d50c4ed7445fd92db77281de29a7c4f7
Twixes <dev@twixes.com>
2020-06-22 20:01:35 +0200

    Improve error message whitespace

--- src/github-api.ts
+++ src/github-api.ts
@@ -56,7 +56,7 @@ async function checkForProblem(response: Response, ignoreStatuses: number[] = []
       // eslint-disable-next-line no-irregular-whitespace
       `GitHub rate limit exceeded for now. Try again in ${deltaMinutes} min ` +
       // eslint-disable-next-line no-irregular-whitespace
-      `at ${rateLimitReset.getHours() % 12 || 12}:${rateLimitReset.getMinutes().toString().padStart(2, '0')} ` +
+      `at ${rateLimitReset.getHours() % 12 || 12}:${rateLimitReset.getMinutes().toString().padStart(2, '0')} ` +
       `${rateLimitReset.getHours() >= 12 ? 'PM' : 'AM'}.`
     )
   } else if (!response.ok && !ignoreStatuses.includes(response.status)) {
