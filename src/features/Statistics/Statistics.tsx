4c1a9b4cfd76e0103ec48c2ad4570b705aecbced
Twixes <dev@twixes.com>
2020-06-23 02:24:48 +0200

    Enhance Statistics loading behavior

--- src/features/Statistics/Statistics.tsx
+++ src/features/Statistics/Statistics.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { useRecoilValue, useSetRecoilState, useRecoilState } from 'recoil'
+import { useRecoilValue, useRecoilState } from 'recoil'
 import { AnimatePresence, motion, Variants } from 'framer-motion'
 import { errorMessageState, currentUserState, eventsState, userEventsState } from '../../atoms'
 import { fetchUserEventsAll } from '../../github-api'
 import { Params } from '../../components/App'
 import Graph, { DataPoint } from '../../components/Graph'
 import './Statistics.scss'
+import Spinner from '../../components/Spinner'
 
 const hourNumbers: number[] = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 const HOURS: string[] = hourNumbers.map(hour => `${hour} AM`).concat(hourNumbers.map(hour => `${hour} PM`))
@@ -23,41 +24,53 @@ const VARIANTS: Variants = {
 export default function Statistics(): JSX.Element {
   const { login } = useParams<Params>()
 
-  const setErrorMessage = useSetRecoilState(errorMessageState)
+  const [errorMessage, setErrorMessage] = useRecoilState(errorMessageState)
   const currentUser = useRecoilValue(currentUserState)
   // const [userEvents, setUserEvents] = useRecoilState(userEventsState({ login: login!.toLowerCase() }))
   const [events, setEvents] = useRecoilState(eventsState)
+  const [wereEventsLoaded, setWereEventsLoaded] = useState<boolean>(false)
 
   useEffect(() => {
     if (currentUser && !events[currentUser.login.toLowerCase()]) {
+      setWereEventsLoaded(false)
       fetchUserEventsAll(currentUser.login).then(newEvents => {
         setEvents({ ...events, [currentUser.login.toLowerCase()]: newEvents })
+        setWereEventsLoaded(true)
       }).catch((error: Error) => {
         setErrorMessage(error.message)
+        setWereEventsLoaded(false)
       })
     }
-    if (currentUser) console.log(events[currentUser.login.toLowerCase()]?.length)
   }, [setErrorMessage, currentUser, events, setEvents])
 
-  const [byHourPoints]: DataPoint[][] = useMemo(() => {
+  const [byHourPoints] = useMemo(() => {
     const byHourPointsTentative: DataPoint[] = HOURS.map(hour => [hour, 0])
-    console.log(currentUser)
     if (currentUser && events[currentUser.login.toLowerCase()]) {
       for (const event of events[currentUser.login.toLowerCase()]!) {
         byHourPointsTentative[event.created_at.getUTCHours()][1] += 1
       }
     }
+    byHourPointsTentative.push(...byHourPointsTentative.splice(0, 6))
     return [byHourPointsTentative]
   }, [events, currentUser])
 
   return (
     <AnimatePresence>
-      {!currentUser ? null : <motion.div
-        className="Statistics" variants={VARIANTS} initial="hidden" animate="shown" exit="hidden" positionTransition
-      >
-        <h1>By hour</h1>
-        <Graph dataPoints={byHourPoints}/>
-      </motion.div>}
+      {(() => {
+        if (errorMessage) return null
+        else if (!currentUser) return <Spinner/>
+        else {
+          return (
+            <motion.div
+              className="Statistics" variants={VARIANTS}
+              initial="hidden" animate="shown" exit="hidden" positionTransition
+            >
+              <h1>By hour</h1>
+              <Graph dataPoints={byHourPoints} isLoading={!wereEventsLoaded}/>
+            </motion.div>
+          )
+        }
+      })()}
     </AnimatePresence>
   )
 }
