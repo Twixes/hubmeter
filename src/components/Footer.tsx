907dcc39ddac0974d3bbe09043f1adb33f6797fb
Twixes <dev@twixes.com>
2020-06-23 02:23:10 +0200

    Add framer-motion positionTransition to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './Footer.scss'
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="Footer">
+    <motion.footer className="Footer" positionTransition>
       <span>GitHub&nbsp;activity insights you&nbsp;haven't seen&nbsp;before. <br/>
         By&nbsp;<a href="https://twixes.com">Twixes</a>.&nbsp;👋
       </span>
-    </footer>
+    </motion.footer>
   )
 }
