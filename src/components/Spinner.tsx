a17267b0b5de6cfb7eb8eed3b24eabcb2bb383da
Twixes <dev@twixes.com>
2020-06-23 02:24:26 +0200

    Add Spinner for loading animations

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { motion, Variants } from 'framer-motion'
+import './Spinner.scss'
+
+interface Props {
+  color?: string
+}
+
+const VARIANTS: Variants = {
+  hidden: {
+    opacity: 0
+  },
+  shown: {
+    opacity: 1
+  }
+}
+
+export default function Spinner({ color }: Props): JSX.Element {
+  return (
+    <motion.div className="Spinner-container" variants={VARIANTS} initial="hidden" animate="shown" exit="hidden">
+      <div className="Spinner" style={color ? { borderLeftColor: color, borderRightColor: color } : {}}/>
+    </motion.div>
+  )
+}
