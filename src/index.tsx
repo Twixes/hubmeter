03366452714fad87a381e0c5c95a2298241d5046
Twixes <dev@twixes.com>
2020-06-25 00:44:40 +0200

    Integrate Sentry

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
-import App from './components/App'
-import 'focus-visible'
+import * as Sentry from '@sentry/browser';
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
 import 'sanitize.css/typography.css'
+import 'focus-visible'
+import App from './components/App'
 import './index.scss'
+
+Sentry.init({dsn: "https://eaa5d3bc32f640ceb2f8090656940497@o173795.ingest.sentry.io/5288711"})
 
 ReactDOM.render(
   <React.StrictMode>
