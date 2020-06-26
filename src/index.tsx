3f29cd87ec30ab31f1b2512b766e9a3e2643bd15
Twixes <dev@twixes.com>
2020-06-27 01:59:31 +0200

    Add PostHog for analytics and react-cookie for cookie hooks

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
-import * as Sentry from '@sentry/browser';
+import { CookiesProvider } from 'react-cookie'
+import * as Sentry from '@sentry/browser'
+import posthog from 'posthog-js'
 import 'sanitize.css'
 import 'sanitize.css/forms.css'
 import 'sanitize.css/typography.css'
@@ -10,14 +12,17 @@ import 'focus-visible'
 import App from './components/App'
 import './index.scss'
 
-Sentry.init({dsn: "https://eaa5d3bc32f640ceb2f8090656940497@o173795.ingest.sentry.io/5288711"})
+Sentry.init({ dsn: "https://eaa5d3bc32f640ceb2f8090656940497@o173795.ingest.sentry.io/5288711" })
+posthog.init('AfWAhZKo9pPPH8blum1SKbPAWPixyRGOuaxC_laQNAc', { api_host: 'https://app.posthog.com' })
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <BrowserRouter>
-        <App/>
-      </BrowserRouter>
+      <CookiesProvider>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+      </CookiesProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/types/posthog-js.d.ts
@@ -0,0 +1,3 @@
+declare module 'posthog-js' {
+  export function init(token: string, config: object, name?: string): object
+}
