e4197d2556e7ef839aeb9d800cd456867e34dcc4
Twixes <dev@twixes.com>
2020-06-27 03:09:25 +0200

    Optimize graph drawing and add markers

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,10 +1,9 @@
-import React, { useMemo, useState, Dispatch, SetStateAction, useEffect } from 'react'
+import React, { useMemo, useState, Dispatch, SetStateAction } from 'react'
 import useSize from '@react-hook/size'
-import { motion, AnimatePresence, Variants, useMotionValue, MotionValue } from 'framer-motion'
+import { motion, AnimatePresence, Variants } from 'framer-motion'
 import { roundCommands, SVGCommand } from 'svg-round-corners'
 import './Graph.scss'
 import Spinner from './Spinner'
-import { type } from 'os'
 
 export type DataPoint = [Date | string | number, number]
 type CaptionPoint = [Date | string | number, number, number]
@@ -26,69 +25,68 @@ const GRAPH_VARIANTS: Variants = {
   }
 }
 
-function drawRoundedBarGraphMain(points: DataPoint[], width: number, height: number): JSX.Element {
-  if (!points.length) throw Error('No data points to draw.')
+function drawRoundedBarGraphWithOverlay(
+  points: DataPoint[], width: number, height: number, setCaptionPoint: Dispatch<SetStateAction<CaptionPoint | null>>
+): [JSX.Element, JSX.Element[]] {
+  if (!points.length) throw Error('No data points to draw')
   const columnWidth: number = width / points.length
-  const heightWithoutBaseline: number = height - CORNER_RADIUS
   const maxY: number = Math.max(...points.map(([, y]) => y)) || Infinity // fall back to 1 to avoid division by 0
+  const markerElements: JSX.Element[] = []
+  const markerSpace: number = 10 ** Math.floor(Math.log10(maxY)) * height / maxY
+  for (let markerY = height - 1; markerY > 0; markerY -= markerSpace) {
+    markerElements.push(<rect className="Graph-main-marker" x={0} y={Math.round(markerY)} width={width} height={1} />)
+  }
   let xPosition: number = 0
   let yPosition: number = height
-  const commands: SVGCommand[] = [
-    { marker: 'M', values: { x: width, y: yPosition } },
+  const overlayElements: JSX.Element[] = []
+  const mainCommands: SVGCommand[] = [
+    { marker: 'M', values: { x: width, y: yPosition + 1 } },
     { marker: 'L', values: { x: xPosition, y: yPosition } }
   ]
-  for (const [, y] of points) {
-    yPosition = (1 - y / maxY) * heightWithoutBaseline
-    commands.push({ marker: 'L', values: { x: xPosition, y: yPosition } })
-    xPosition += columnWidth
-    commands.push({ marker: 'L', values: { x: xPosition, y: yPosition } })
-  }
-  commands.push({ marker: 'Z', values: { x: xPosition, y: yPosition } })
-  return (
-    <svg
-      viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg"
-      width={width} height={height}
-    >
-      <path d={roundCommands(commands, CORNER_RADIUS, 3).path}/>
-    </svg>
-  )
-}
-
-function drawRoundedBarGraphOverlay(
-  points: DataPoint[], width: number, height: number, setCaptionPoint: Dispatch<SetStateAction<CaptionPoint | null>>
-): JSX.Element[] {
-  if (!points.length) throw Error('No data points to draw.')
-  const columnWidth: number = width / points.length
-  const heightWithoutBaseline: number = height - CORNER_RADIUS
-  const maxY: number = Math.max(...points.map(([, y]) => y)) || Infinity // fall back to 1 to avoid division by 0
-  const elements: JSX.Element[] = []
   for (const [i, [x, y]] of points.entries()) {
-    const commands: SVGCommand[] = [
+    const overlayCommands: SVGCommand[] = [
       { marker: 'M', values: { x: columnWidth, y: height } },
       { marker: 'L', values: { x: 0, y: height } }
     ]
-    const yPosition: number = (1 - y / maxY) * heightWithoutBaseline
-    commands.push(...[
+    yPosition = (1 - y / maxY) * height
+    mainCommands.push({ marker: 'L', values: { x: xPosition, y: yPosition } })
+    xPosition += columnWidth
+    overlayCommands.push(...[
       { marker: 'L', values: { x: 0, y: yPosition } },
       { marker: 'L', values: { x: columnWidth, y: yPosition } },
       { marker: 'Z', values: { x: columnWidth, y: yPosition } }
     ])
-    elements.push(
-      <div
+    mainCommands.push({ marker: 'L', values: { x: xPosition, y: yPosition } })
+    overlayElements.push(
+      <svg
+        viewBox={`0 0 ${columnWidth} ${height}`} xmlns="http://www.w3.org/2000/svg"
+        width={columnWidth} height={height} fill="transparent"
         className="Graph-overlay-part" key={`Graph-overlay-part-${i}`} style={{ width: columnWidth }}
         onMouseEnter={() => { setCaptionPoint([x, y, i]) }} onMouseLeave={() => { setCaptionPoint(null) }}
       >
-        <svg
-          viewBox={`0 0 ${columnWidth} ${height}`} xmlns="http://www.w3.org/2000/svg"
-          width={columnWidth} height={height} fill="transparent"
-        >
-          <path d={roundCommands(commands, CORNER_RADIUS, 3).path}/>
-        </svg>
-        <div className="Graph-overlay-legend-x-container"><div className="Graph-overlay-legend-x">{x}</div></div>
-      </div>
+        <path d={roundCommands(overlayCommands, CORNER_RADIUS).path}/>
+      </svg>
     )
   }
-  return elements
+  mainCommands.push({ marker: 'Z', values: { x: xPosition, y: yPosition } })
+  return [(
+    <svg viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
+      {markerElements}<path d={roundCommands(mainCommands, CORNER_RADIUS).path}/>
+    </svg>
+  ), overlayElements]
+}
+
+function generateXLegend(points: DataPoint[], width: number): JSX.Element {
+  if (!points.length) throw Error('No data points to draw.')
+  const columnWidth: number = width / points.length
+  const elements: JSX.Element[] = points.map(([x,]) => (
+    <div className="Graph-legend-x-value-container" style={{ width: columnWidth }}>
+      <div className="Graph-legend-x-value">{x}</div>
+    </div>
+  ))
+  return (
+    <div className="Graph-legend-x">{elements}</div>
+  )
 }
 
 export default function Graph({ dataPoints, isLoading }: Props): JSX.Element {
@@ -96,9 +94,9 @@ export default function Graph({ dataPoints, isLoading }: Props): JSX.Element {
   const [vectorWidth, vectorHeight] = useSize(vectorRef)
   const [captionPoint, setCaptionPoint] = useState<CaptionPoint | null>(null)
 
-  const [vectorMainElement, vectorOverlayElements] = useMemo(() => isLoading ? Array(2).fill(null) : [
-    drawRoundedBarGraphMain(dataPoints, vectorWidth, vectorHeight),
-    drawRoundedBarGraphOverlay(dataPoints, vectorWidth, vectorHeight, setCaptionPoint)
+  const [vectorMainElement, vectorOverlayElements, xLegendElement] = useMemo(() => isLoading ? Array(3).fill(null) : [
+    ...drawRoundedBarGraphWithOverlay(dataPoints, vectorWidth, vectorHeight, setCaptionPoint),
+    generateXLegend(dataPoints, vectorWidth)
   ], [dataPoints, isLoading, vectorWidth, vectorHeight])
 
   return (
@@ -106,11 +104,12 @@ export default function Graph({ dataPoints, isLoading }: Props): JSX.Element {
       <div className="Graph-container" ref={vectorRef}>
         <AnimatePresence>
           {isLoading ? <Spinner color="var(--color-accent)"/> : (
-            <motion.div
-              className="Graph-subcontainer" variants={GRAPH_VARIANTS} initial="hidden" animate="shown" exit="hidden"
-              transition={{ type: 'spring', damping: 18 }}
-            >
-              <figure className="Graph-main">{vectorMainElement}</figure>
+            <>
+              <motion.figure
+                className="Graph-main" variants={GRAPH_VARIANTS} initial="hidden" animate="shown" exit="hidden"
+                transition={{ type: 'spring', damping: 18 }}
+              >{vectorMainElement}
+              </motion.figure>
               <figure className="Graph-overlay">
                 <AnimatePresence>
                   {captionPoint && <motion.figcaption
@@ -135,7 +134,8 @@ export default function Graph({ dataPoints, isLoading }: Props): JSX.Element {
                 </AnimatePresence>
                 {vectorOverlayElements}
               </figure>
-            </motion.div>)}
+              {xLegendElement}
+            </>)}
         </AnimatePresence>
       </div>
     </div>
