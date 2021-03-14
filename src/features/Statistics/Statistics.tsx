efeba7242aeb9cc0ec1368da523b3bb6664cef82
Michael Matloka <dev@twixes.com>
2021-03-14 07:34:01 +0100

    Fix aggregation import

--- src/features/Statistics/Statistics.tsx
+++ src/features/Statistics/Statistics.tsx
@@ -15,8 +15,8 @@ import {
 import { Params } from '../../components/App'
 import Graph from '../../components/Graph'
 import Spinner from '../../components/Spinner'
+import { aggregateByDayOfWeek, aggregateByHour } from '../../data-processing/aggregation'
 import { fetchUserEventsAll } from '../../github-api'
-import { aggregateByDayOfWeek, aggregateByHour } from './data-processing'
 
 const HOUR_NUMBERS: number[] = [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 const HOURS: string[] = HOUR_NUMBERS.map((hour) => `${hour} AM`).concat(HOUR_NUMBERS.map((hour) => `${hour} PM`))
