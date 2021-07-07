d5b920aaa305fd9acdbdd8b9e0549270b782f146
Michael Matloka <dev@twixes.com>
2021-07-07 13:12:55 +0200

    Reword tagline

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,7 +26,7 @@ export default function Footer(): JSX.Element {
     return (
         <motion.footer css={[widthControl, footer]}>
             <span>
-                GitHub&nbsp;activity insights you&nbsp;haven't seen&nbsp;before. <br />
+                GitHub&nbsp;activity stats you&nbsp;haven't seen&nbsp;before. <br />
                 By&nbsp;<a href="https://matloka.com">Michael Matloka</a>.&nbsp;👋
             </span>
         </motion.footer>
